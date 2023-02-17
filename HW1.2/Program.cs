1a89329e2a39faa6e2e17696cd85dff86f3f613a
Danil Neklyudov <dejeksdan@gmail.com>
2023-02-17 13:25:07 +0500

    Problem 6 solved

---
+++ HW1.2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 6
+
+Console.WriteLine("Введите 1ое число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+if (numb % 2 == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+
