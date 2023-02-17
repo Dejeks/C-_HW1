4dc0a42e87dd102bac4010efef5a7649117aa14c
Danil Neklyudov <dejeksdan@gmail.com>
2023-02-17 13:24:57 +0500

    Problem 4 solved

---
+++ HW1.1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 4
+
+Console.WriteLine("Введите 1ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 3ое число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите 3ое число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a >= b & a >= c)
+{
+    Console.WriteLine(a);
+}
+else if (b >= a & b >= c)
+{
+    Console.WriteLine(b);
+}
+else
+    Console.WriteLine(c);
