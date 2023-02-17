e0936c7e4c19c9546f8b2b43f0a71db3df0336d9
Danil Neklyudov <dejeksdan@gmail.com>
2023-02-17 13:31:01 +0500

    bugfix in hw1.3

--- HW1.3/Program.cs
+++ HW1.3/Program.cs
@@ -1,8 +1,8 @@
 ﻿//Задача 8
+Console.WriteLine("Введите число: ");
+int numb = Convert.ToInt32(Console.ReadLine());
 
-numb = Convert.ToInt32(Console.ReadLine());
-
-for (int i = 0; i <= numb; i++)
+for (int i = 1; i <= numb; i++)
 {
     if(i % 2 == 0) {Console.WriteLine(i);}
 }
