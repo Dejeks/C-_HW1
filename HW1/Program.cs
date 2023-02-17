6b6af57dbec0595e7a2c88dc8d900d9ac0dac403
Danil Neklyudov <dejeksdan@gmail.com>
2023-02-17 13:23:22 +0500

    Problem 2 solved

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2
+
+Console.WriteLine("Введите 1ое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2ое число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
+
+
