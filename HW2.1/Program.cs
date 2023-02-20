c3f9e28f12ac527666c3dbf62dbc4b63fc2d6870
Danil Neklyudov <dejeksdan@gmail.com>
2023-02-20 22:41:00 +0500

    Problem 10 solved

--- HW2.1/Program.cs
+++ HW2.1/Program.cs
@@ -2,3 +2,16 @@
 Напишите программу, которая принимает на вход 
 трёхзначное число и на выходе показывает вторую цифру этого числа.*/
 
+int numb = 0;
+
+while (true)
+{
+    numb = Convert.ToInt32(Console.ReadLine());
+    if (numb % 1000 == numb & numb % 100 != numb) //Условие 3х значности
+    {
+        numb /= 10;
+        Console.WriteLine(numb % 10);
+    }
+    else{Console.WriteLine("Введите 3х значеной число!");}
+    
+}
